77c048ca303c21a77fc16333e4ebe16499d1debe
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-16 11:49:27 -0300

    feat: create auth or app

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,18 +1,18 @@
-import '../common/template/dependencies'
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
 import Messages from '../common/msg/messages'
 
+import Routes from './routes'
+
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
-        <div className='content-wrapper'> 
-            {props.children}
-        </div>
+        <Routes/>
         <Footer />
         <Messages />
     </div>
