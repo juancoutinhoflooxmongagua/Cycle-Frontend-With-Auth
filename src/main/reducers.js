74c4f36639c88f313b7d4e8a022da68d9735a71f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-16 10:02:24 -0300

    feat: add import

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
-
+import { AuthReducer } from '../auth/authReducer'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
@@ -11,7 +11,8 @@ const rootReducer = combineReducers({
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
