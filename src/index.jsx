5a9a9609252f59fe227dc3bd23199e68b760e7f7
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-16 10:58:34 -0300

    feat: add actions for requests

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
-
 import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
-
-import Routes from './main/routes'
 import reducers from './main/reducers'
+import AuthOrApp from './main/authOrApp'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
       && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <AuthOrApp />
     </Provider>
 , document.getElementById('app'))
